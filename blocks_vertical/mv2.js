683a59e484f21bed2f0f4bf218aa16acbef05b04
Sandy Enoch <alexander@robotical.io>
2020-05-13 00:36:04 +0100

    Add marty demo sensor block

--- blocks_vertical/mv2.js
+++ blocks_vertical/mv2.js
@@ -6,6 +6,61 @@ goog.require('Blockly.Colours');
 goog.require('Blockly.constants');
 goog.require('Blockly.ScratchBlocks.VerticalExtensions');
 
+Blockly.Blocks['mv2_demo_sensor'] = {
+  /**
+   * Block to give readout of demo sensor
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": "%1 %2 Demo sensor",
+      "category": Blockly.Categories.mv2,
+      "checkboxInFlyout": true,
+      "args0": [
+        {
+          "type": "field_image",
+          "src": Blockly.mainWorkspace.options.pathToMedia + "extensions/marty-small.svg",
+          "width": 40,
+          "height": 40
+        },
+        {
+          "type": "field_vertical_separator"
+        }
+      ],
+      "extensions": ["colours_mv2", "output_number"]
+    });
+  }
+};
+
+Blockly.Blocks['mv2_set_demo_sensor'] = {
+  /**
+   * Block to set demo sensor value
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": "%1 %2 Set Demo sensor to %3",
+      "category": Blockly.Categories.mv2,
+      "args0": [
+        {
+          "type": "field_image",
+          "src": Blockly.mainWorkspace.options.pathToMedia + "extensions/marty-small.svg",
+          "width": 40,
+          "height": 40
+        },
+        {
+          "type": "field_vertical_separator"
+        },
+        {
+          "type": "input_value",
+          "name": "SENSORVAL"
+        }
+      ],
+      "extensions": ["colours_mv2", "shape_statement"]
+    });
+  }
+};
+
 Blockly.Blocks['mv2_set_ip'] = {
   /**
    * Block to set Marty IP for REST commands
